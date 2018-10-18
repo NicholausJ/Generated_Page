82d7309f275a045cff51c8e025341c49d4d65082
Nick J <nicholausj@gmail.com>
2018-10-18 13:07:35 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
-console.log('This would be the main JS file.');
+/*console.log('This would be the main JS file.');*/
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction1() {
