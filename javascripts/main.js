5acc99550ed9c13ecbd7c6cbd7541ab74ffb47d4
Nick J <nicholausj@gmail.com>
2018-10-18 12:58:45 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
 console.log('This would be the main JS file.');
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction1() {
+  var x = document.getElementById("my-topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
